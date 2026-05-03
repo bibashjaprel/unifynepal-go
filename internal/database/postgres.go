4c1e2a5370a30336e1593725940d3a260f0d3084
Bibash Japrel <bibashjaprel111@gmail.com>
2026-05-04 00:11:05 +0545

    Implement PostgreSQL database connection functionality

---
+++ internal/database/postgres.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func Connect(databaseURL string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(databaseURL), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect database: ", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to get sql DB: ", err)
+	}
+
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetMaxOpenConns(20)
+	sqlDB.SetConnMaxLifetime(30 * time.Minute)
+
+	return db
+}
