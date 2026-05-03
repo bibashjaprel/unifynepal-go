5452af503d181546f81d4ef37137d4027b20ea99
Bibash Japrel <bibashjaprel111@gmail.com>
2026-05-04 00:28:30 +0545

    Add JWT utility functions for token generation and claims management

---
+++ internal/utils/jwt.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type JWTClaims struct {
+	UserID uuid.UUID `json:"user_id"`
+	Email  string    `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func GenerateToken(userID uuid.UUID, email string, secret string, expiresInHours int) (string, error) {
+	claims := JWTClaims{
+		UserID: userID,
+		Email:  email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Duration(expiresInHours) * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
